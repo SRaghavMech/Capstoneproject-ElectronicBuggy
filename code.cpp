0d5c1570e22b486432cbf7fa3481e00fde31230c
SRaghavMech <87862054+SRaghavMech@users.noreply.github.com>
2021-07-24 00:22:32 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,13 @@
+int Left;
+int Right;
+int time;
+void setup()  
+{    
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(A0, INPUT);
+    pinMode(A2, INPUT);
+    time=millis();
+} 
